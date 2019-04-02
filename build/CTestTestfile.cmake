2d386e8be4cbd6f0861bdfaaf62e0d80c810f326
Ivan Savelyev <s.i.v.892@gmail.com>
2019-04-03 00:53:54 +0300

    fix bin file name

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/isavel/Nextcloud/Coding/С++/Otus/Month_1/Lesson_1/otus_lesson_1
-# Build directory: /home/isavel/Nextcloud/Coding/С++/Otus/Month_1/Lesson_1/otus_lesson_1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(helloworld_tests "test_version")
-set_tests_properties(helloworld_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/isavel/Nextcloud/Coding/С++/Otus/Month_1/Lesson_1/otus_lesson_1/CMakeLists.txt;63;add_test;/home/isavel/Nextcloud/Coding/С++/Otus/Month_1/Lesson_1/otus_lesson_1/CMakeLists.txt;0;")
