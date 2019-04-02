828b4bcc5460ae66e6321cbeaf1a91b28e7616fd
Ivan Savelyev <s.i.v.892@gmail.com>
2019-04-03 00:40:56 +0300

    delete double show build number

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(helloworld_tests "test_version")
-set_tests_properties(helloworld_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/isavel/Nextcloud/Coding/С++/Otus/Month_1/Lesson_1/otus_lesson_1/CMakeLists.txt;65;add_test;/home/isavel/Nextcloud/Coding/С++/Otus/Month_1/Lesson_1/otus_lesson_1/CMakeLists.txt;0;")
+set_tests_properties(helloworld_tests PROPERTIES  _BACKTRACE_TRIPLES "/home/isavel/Nextcloud/Coding/С++/Otus/Month_1/Lesson_1/otus_lesson_1/CMakeLists.txt;63;add_test;/home/isavel/Nextcloud/Coding/С++/Otus/Month_1/Lesson_1/otus_lesson_1/CMakeLists.txt;0;")
--- lib.cpp
+++ lib.cpp
@@ -4,6 +4,5 @@
 
 int version()
 {
-  std::cout << "PROJECT_VERSION_PATCH: " << PROJECT_VERSION_PATCH;
   return PROJECT_VERSION_PATCH;
 }
